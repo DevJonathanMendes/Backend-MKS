68ac16748521a522106ba791d46b90661496431f
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2024-04-23 15:05:57 -0300

    refactor: improvement and comments

--- src/app.pipe.ts
+++ src/app.pipe.ts
@@ -1,22 +1,28 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
 
+// É um PipeTransform para remover espaços desnecessários das strings.
+// Ex: " Hello  World!  " >> "Hello World!"
 @Injectable()
 export class AppPipeTransform implements PipeTransform {
   transform(data: Record<string, any>) {
     const sanitizedData: Record<string, any> = {};
+    if (typeof data === 'object') {
+      Object.entries(data).forEach(([key, value]) => {
+        sanitizedData[key] = this.sanitizeValue(value);
+      });
 
-    Object.entries(data).forEach(([key, value]) => {
-      sanitizedData[key] = this.sanitizeValue(value);
-    });
-
-    return sanitizedData;
-  }
-
-  private sanitizeValue(value: string): string {
-    if (typeof value === 'string') {
-      value = value.replace(/\s{2,}(?![\d\s]*$)/g, ' ').trim();
+      return sanitizedData;
     }
 
-    return value;
+    return data;
+  }
+
+  // Função primordial para remover os espaços, ela usa uma Expressão Regular.
+  private sanitizeValue(value: string): string {
+    const regexp = /\s{2,}(?![\d\s]*$)/g; // Expressão Regular.
+
+    return typeof value === 'string'
+      ? value.replace(regexp, ' ').trim()
+      : value;
   }
 }

a0dbc914e9acb4b41354eab4875ab36e91edc09c
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2024-04-23 15:05:34 -0300

    refactor: improvement TypeORM database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeORMExceptionFilter } from './app.filter';
+
+import { MovieEntity } from './movies/entities/movie.entity';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MoviesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [MovieEntity],
+      synchronize: true,
+    }),
+
+    MoviesModule,
+  ],
+  providers: [
+    {
+      provide: 'APP_FILTER',
+      useClass: TypeORMExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}

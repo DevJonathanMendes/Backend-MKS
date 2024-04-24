b303b95ace131928a5a12e09f85dcc50712f7246
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2024-04-24 10:39:36 -0300

    feat: add AuthModule and UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,11 @@ import { TypeORMExceptionFilter } from './app.filter';
 import { MovieEntity } from './movies/entities/movie.entity';
 import { MoviesModule } from './movies/movies.module';
 
+import { UserEntity } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
+
+import { AuthModule } from './auth/auth.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -16,11 +21,12 @@ import { MoviesModule } from './movies/movies.module';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [MovieEntity],
+      entities: [UserEntity, MovieEntity],
       synchronize: true,
     }),
-
+    AuthModule,
     MoviesModule,
+    UsersModule,
   ],
   providers: [
     {

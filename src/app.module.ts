c83d3721a55f76b64945b6c27b6043a846b1ce51
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2024-04-25 18:02:02 -0300

    feat: add caching/redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TypeORMExceptionFilter } from './app.filter';
+import { redisStore } from 'cache-manager-redis-yet';
+import { RedisClientOptions } from 'redis';
 
 import { MovieEntity } from './movies/entities/movie.entity';
 import { MoviesModule } from './movies/movies.module';
@@ -10,6 +13,19 @@ import { UserEntity } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 
 import { AuthModule } from './auth/auth.module';
+
+import { TypeORMExceptionFilter } from './app.filter';
+
+const SUPABASE_INDIA = {
+  type: 'postgres',
+  host: 'aws-0-eu-west-1.pooler.supabase.com',
+  port: 5432,
+  username: 'postgres.uerhunzlodwpyvwaogfm',
+  password: 'umasenhaforte',
+  database: 'postgres',
+  entities: [UserEntity, MovieEntity],
+  synchronize: true,
+};
 
 @Module({
   imports: [
@@ -24,14 +40,31 @@ import { AuthModule } from './auth/auth.module';
       entities: [UserEntity, MovieEntity],
       synchronize: true,
     }),
+    CacheModule.register<RedisClientOptions>({
+      isGlobal: true,
+      ttl: 1000 * 60 * 60,
+      store: async () =>
+        await redisStore({
+          ttl: 1000 * 60 * 60,
+          password: process.env.REDIS_PASSWORD,
+          socket: {
+            host: 'localhost',
+            port: Number(process.env.REDIS_PORT_NUMBER),
+          },
+        }),
+    }),
     AuthModule,
-    MoviesModule,
     UsersModule,
+    MoviesModule,
   ],
   providers: [
     {
       provide: 'APP_FILTER',
       useClass: TypeORMExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
     },
   ],
 })
